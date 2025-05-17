03ca5cc3ba57c72e1f345185a7d19c971062e0a8
AnuarOrozco <anuarorozzco@gmail.com>
2025-05-16 19:07:17 -0600

    refactor: normalize main.jsx with routing, query client, and animation setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// React Query Configgg ;v
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
+// Framer Motion Config
+import { AnimatePresence } from 'framer-motion'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AnimatePresence mode='wait'>
+          <App />
+        </AnimatePresence>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
